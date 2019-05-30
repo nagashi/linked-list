5ea87aaf773649eb9156a19dd05d976448c53dab
Charles ORiley <ceoriley@gmail.com>
2019-05-29 19:38:27 -0500

    Linded List 2

---
+++ linked-list-2.rb
@@ -0,0 +1,39 @@
+class LinkedListNode
+  attr_accessor :value, :next_node
+  
+
+  def initialize(value, next_node=nil)
+    @value = value
+    @next_node = next_node
+  end
+
+  def reverse_list(list, previous=nil)
+    if !list
+      previous
+    else
+      current_head = list.next_node
+      list.next_node = previous
+      reverse_list(current_head, list)
+    end
+  end
+
+  def print_values(list_node)
+    if list_node
+      print "#{list_node.value} --> "
+      print_values(list_node.next_node)
+    else
+      print "nil\n"
+      return
+    end
+  end
+end
+
+node1 = LinkedListNode.new(37)
+node2 = LinkedListNode.new(99, node1)
+node3 = LinkedListNode.new(12, node2)
+
+node3.print_values(node3)
+
+puts "------------------------"
+
+node3.print_values(node3.reverse_list(node3))
