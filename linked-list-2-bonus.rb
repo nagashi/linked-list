a733484207eb147238d9a2ce9e6f19c78351454f
Charles ORiley <ceoriley@gmail.com>
2019-06-01 17:57:35 -0500

    Data Structures—Linked List #2 Bonus

--- linked-list-2-bonus.rb
+++ linked-list-2-bonus.rb
@@ -9,6 +9,7 @@ class LinkedListNode
   def infinite_linked_list?(list)
     runner1 = list.next_node
     runner2 = list.next_node
+    cycle = 0
   
     until runner1.nil?
       runner1 = runner1.next_node
@@ -16,7 +17,8 @@ class LinkedListNode
   
       runner1 = runner1.next_node
       runner2 = runner2.next_node
-      return true if runner1 == runner2
+      cycle += 1
+      return "true in #{cycle} cycles" if runner1 == runner2
     end
     false
   end
